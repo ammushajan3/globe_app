fcb9cf94324e6ec60aba8e9222be193feba7b2bd
ammushajan3 <ammu.shajan@moonraft.com>
2020-12-30 11:07:37 +0530

    dashboard drawer and bottomnav added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:task_app/themes/color.dart';
 import 'package:task_app/themes/themes.dart';
 import 'package:task_app/widgets/dashboard_app_bar.dar.dart';
 import 'package:task_app/widgets/dashboard_body.dart';
+import 'package:task_app/widgets/dashboard_drawer.dart';
 
 void main() {
   runApp(
@@ -18,37 +20,27 @@ class Dashboard extends StatelessWidget {
     return Scaffold(
       appBar: DashboardAppBar(),
       drawer: Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text('My prepaid'),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-          ),
-          ListTile(
-            title: Text('About Globe'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            title: Text('Payment'),
-            onTap: () {
-              // Update the state of the app
-              // ...
-              // Then close the drawer
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      ),
+        child: DashboardDrawer(),
       ),
       body:DashboardBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: AppColors.COLOR_LIGHT_BLUE,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books),
+            label: 'Library',
+          ),
+        ],
+        selectedItemColor: AppColors.COLOR_BLACK,
+      ),
     );
   }
 }
---
+++ lib/widgets/dashboard_drawer.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DashboardDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('My prepaid'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: Text('About Globe'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text('Payment'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
